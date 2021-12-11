2b39cb6e8aeb4625bab14c6dc8b1ae043f8a8e25
Kristijan Celic <KristijanCelic@windata.de>
2021-12-11 08:58:01 +0100

    commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MoqBug
+{
+    public interface IHardware
+    {
+        void Transmit(IntPtr ptr, byte[] send, byte[] recv);
+    }
+
+    public class MyImpl : IHardware
+    {
+        public void Transmit(IntPtr ptr, byte[] send, byte[] recv)
+        {
+            Console.WriteLine(ptr);
+            Console.WriteLine(BitConverter.ToString(send));
+            Console.WriteLine(BitConverter.ToString(recv));
+        }
+    }
+
+    public class MyModule
+    {
+        internal IHardware hw = new MyImpl();
+
+        public void DoWork(byte[] send)
+        {
+            //... here is some complicated stuff to determine the send-buffer
+
+            hw.Transmit((IntPtr)1, send, new byte[8]);
+        }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+
+namespace MoqBug
+{
+    public class Tests
+    {
+        private MyModule _sut;
+
+        [Test]
+        public void MyTest_Works()
+        {
+            _sut = new MyModule();
+
+            Mock<IHardware> hwMock = new Mock<IHardware>();
+            hwMock.Setup(m => m.Transmit(It.IsAny<IntPtr>(), It.IsAny<byte[]>(), It.IsAny<byte[]>()));
+            _sut.hw = hwMock.Object;
+
+            _sut.DoWork(new byte[]{1,2,3});
+
+            hwMock.Verify(m => m.Transmit((IntPtr)1, new byte[]{1,2,3}, It.IsAny<byte[]>()));
+        }
+
+        [Test]
+        public void MyTest_Works_Also()
+        {
+            _sut = new MyModule();
+
+            Mock<IHardware> hwMock = new Mock<IHardware>();
+            hwMock.Setup(m => m.Transmit(It.IsAny<IntPtr>(), It.IsAny<byte[]>(), It.IsAny<byte[]>()));
+            _sut.hw = hwMock.Object;
+
+            _sut.DoWork(new byte[] { 1, 2, 3 });
+
+            string inputAsString = Convert.ToBase64String(new byte[] { 1, 2, 3 });
+
+            var testeeAsByte = Convert.FromBase64String(inputAsString);
+
+            hwMock.Verify(m => m.Transmit((IntPtr)1, testeeAsByte, It.IsAny<byte[]>()));
+        }
+
+        [Test]
+        public void MyTest_Works_Not()
+        {
+            _sut = new MyModule();
+
+            Mock<IHardware> hwMock = new Mock<IHardware>();
+            hwMock.Setup(m => m.Transmit(It.IsAny<IntPtr>(), It.IsAny<byte[]>(), It.IsAny<byte[]>()));
+            _sut.hw = hwMock.Object;
+
+            _sut.DoWork(new byte[] { 1, 2, 3 });
+
+            string inputAsString = Convert.ToBase64String(new byte[] { 1, 2, 3 });
+
+            hwMock.Verify(m => m.Transmit((IntPtr)1, Convert.FromBase64String(inputAsString), It.IsAny<byte[]>()));
+        }
+    }
+}
